651c6c06636216b5d934fa2c84b7166acb874f59
tsl <torerik.lindstrom@gmail.com>
2018-02-17 02:17:39 +0100

    add sentence listening

--- source/assets/js/rco.js
+++ source/assets/js/rco.js
@@ -32,19 +32,32 @@ $('[data-word]').each(function(){
         $('#word-dropdown').css({ left: event.pageX-5, top: event.pageY-5 });
         $('#word-dropdown').show();
 
-        $('#word-dropdown #listen').off('click').on('click', function() {
-            rco.pronounce(wordElement.html());
+        // listening to word
+        $('#word-dropdown #listen-to-word').off('click').on('click', function() {
+            rco.pronounce(wordElement.text());
             $('#word-dropdown').hide();
         });
 
+        // if parent element has 'data-sentence' attribute, make listening to sentence possible
+        if (wordElement.parent().get(0).hasAttribute("data-sentence")){
+            $('#word-dropdown #listen-to-sentence').off('click').on('click', function() {
+                rco.pronounce(wordElement.parent().text());
+                $('#word-dropdown').hide();
+            });
+            $('#word-dropdown #listen-to-sentence').show();
+        } else {
+            $('#word-dropdown #listen-to-sentence').hide();
+        }
+        
+        // if there's a word page, add the 'learn more' button
         if ($(this).attr('data-wordpage')){
-            $('#word-dropdown #learn-more').attr('href', '/rco/words/' + $(this).attr('data-wordpage') + '.html');
+            $('#word-dropdown #learn-more').attr('href', PREFIX + '/words/' + $(this).attr('data-wordpage') + '.html');
             $('#word-dropdown #learn-more').show();
         } else {
             $('#word-dropdown #learn-more').hide();
         }
 
-        // return false blocks the standard context menu
+        // return false to block the standard context menu
         return false;
     });
 });
